83b58b696677e6cf6f24cd5ae13de998ff22f7f2
cuongloveyou <xoahetht1998@gmail.com>
2021-08-05 22:15:06 +0700

    add view sign in out

---
+++ pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-box">
+  <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <h4>
+      <input type="checkbox">
+      Show password
+    </h4>
+    <h5 class="error" hidden="true">
+      Tài khoản/ mật khẩu sai kìa má
+    </h5>
+    <a class="btn-submit" href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+    <h5>
+      <a href="#">Forget your password?</a>
+    </h5>
+    <h5>
+      Don't have an account?
+      <a href="/register">Sign up</a>
+    </h5>
+</div>
+</template>
---
+++ pages/register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-box">
+    <h2>Sign up</h2>
+    <form>
+      <div class="user-box">
+        <input type="text" name="" required="" />
+        <label>Username</label>
+      </div>
+      <div class="user-box">
+        <input type="password" name="" required="" />
+        <label>Password</label>
+      </div>
+      <div class="user-box">
+        <input type="password" name="" required="" />
+        <label>Confirm Password</label>
+      </div>
+      <div class="user-box">
+        <input type="text" name="" required="" />
+        <label>Name</label>
+      </div>
+      <div class="user-box">
+        <input type="email" name="" required="" />
+        <label>Email</label>
+      </div>
+      <h5 class="error" hidden="true">Tài khoản/ mật khẩu sai kìa má</h5>
+      <a class="btn-submit" href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Register
+      </a>
+    </form>
+    <h5>
+      Already signed up??
+      <a href="/login">Sign in</a>
+    </h5>
+  </div>
+</template>
